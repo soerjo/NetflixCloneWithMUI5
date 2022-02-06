dd44ef2ee075ae0c328ddfe015ac97da80c91ed4
soerjo <ryohastomo@gmail.com>
2022-02-07 00:16:01 +0700

    update material ui server side rendering

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # BELAJAR MATERIAL UI
+
+Clone web Netflix.
+materi belajar dari: https://www.youtube.com/playlist?list=PLLhpK5IKtgqnOuRn3xMJDYiqMv9aQM99n
+
+deploy: https://netflix-clone-with-mui-5.vercel.app/
+
+## TEKNIK YG DI PAKAI:
+
+1. NextJS - Typescript
+2. MaterialUI 5
+3. material ui server side rendering
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,37 +6,53 @@ import CssBaseline from "@mui/material/CssBaseline";
 import theme from "../utils/Theme";
 import "../styles/globals.css";
 
-function MyApp({ Component, pageProps }: AppProps) {
+import { CacheProvider, EmotionCache } from "@emotion/react";
+import createEmotionCache from "../utils/createEmotionCache";
+
+const clientSideEmotionCache = createEmotionCache();
+
+interface MyAppProps extends AppProps {
+  emotionCache?: EmotionCache;
+}
+
+function MyApp({
+  Component,
+  emotionCache = clientSideEmotionCache,
+  pageProps,
+}: MyAppProps) {
   return (
     <>
-      <Head>
-        <title>NextFlix-Soerjo</title>
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/apple-touch-icon.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/favicon-32x32.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/favicon-16x16.png"
-        />
-        <link rel="manifest" href="/site.webmanifest" />
-        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
-        <meta name="msapplication-TileColor" content="#da532c" />
-        <meta name="theme-color" content="#ffffff" />{" "}
-      </Head>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <Component {...pageProps} />
-      </ThemeProvider>
+      <CacheProvider value={emotionCache}>
+        <Head>
+          <title>NextFlix-Soerjo</title>
+          <meta name="viewport" content="initial-scale=1, width=device-width" />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicon-16x16.png"
+          />
+          <link rel="manifest" href="/site.webmanifest" />
+          <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+          <meta name="msapplication-TileColor" content="#da532c" />
+          <meta name="theme-color" content="#ffffff" />{" "}
+        </Head>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </CacheProvider>
     </>
   );
 }
